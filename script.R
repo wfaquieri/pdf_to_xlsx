7c27318cdda2d21f6aad5abbfe53d6bbc4229113
Winicius Faquieri <54724688+wfaquieri@users.noreply.github.com>
2022-10-31 20:21:12 -0300

    init commit

--- script.R
+++ script.R
@@ -4,11 +4,6 @@ txt <- pdftools::pdf_text(pdf=edital)
 lines <- pdftools::pdf_text(pdf=edital) |> 
   readr::read_lines()
 
-cat(txt[1])
-
-cat(txt[2])
-
-
 # -------------------------------------------------------------------------
 
 # Páginas de interesse
@@ -19,20 +14,42 @@ ds1 = page_35[28:52] |> stringr::str_squish()
 ds2 = page_36[6:16] |> stringr::str_squish()
 
 
-
-n = length(ds1)
-
-init_ = paste(ds1[1], ds1[2], sep=" ")
-
-for (i in 3:n) {
-  init_ = paste(init_,ds1[i], sep=" ")
+arrumar_dados = function(text){
+  
+  n = length(text)
+  
+  init_ = paste(text[1], text[2], sep=" ")
+  
+  for (i in 3:n) {
+    init_ = paste(init_,text[i], sep=" ")
+  }
+  
+  init_ = init_ |> stringr::str_split("\\. ")
+  
+  tab = tibble::as_tibble(init_[[1]])
+  
+  return(tab)
+  
 }
 
+tab_1 = arrumar_dados(text=ds1)
 
-tab = init_ |> stringr::str_split("\\. ") 
+tab_2 = arrumar_dados(text=ds2)
 
-tab_final = tibble::as_tibble(tab[[1]])
+tab_final = dplyr::bind_rows(tab_1, tab_2)
 
+
+
+
+title_subt = tab_final[1,1] |> dplyr::pull(value)
+
+title = title_subt |> stringr::str_sub(start = 1L, end = 26L)
+subtitle = title_subt |> stringr::str_sub(start = 30L, end = -1L)
+
+tab_final[1,1] = subtitle
+
+colnames(tab_final) = title
+   
 tab_final |> writexl::write_xlsx('Disciplinas_DS_Petrobas.xlsx')
 
 
